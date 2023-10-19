a264608580871aeede18c910de2505600645ef58
Nombre Apellido <email@dominio.com>
2023-10-18 22:58:57 -0500

    third section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-console.log('me estoy ejecutando');
+
+/********************************************/
+/***** portafolio fundamentos academlo *****/
+/******************************************/
+
+function handles () {
+    // Hago la lectura de los elementos del DOM
+    const slider = document.querySelector('.slider__skills');
+    const labels = document.querySelectorAll('.slider__skills img');
+    // Extraigo las rutas de las imagenes que voy a necesitar
+    const images = [];
+    labels.forEach(element => {images.push(element.src)});
+    // Asigno la primera imagen al slider
+    let count = 0;
+    slider.innerHTML = `
+        <img src="${images[count]}" alt="logo habilidad dura">
+    `;
+    // Pongo el slider a cambiar de item automaticamente
+    setInterval(() => {
+        if (count<images.length-1) {
+            count++;
+        } else {
+            count = 0;
+        }
+        slider.innerHTML = `
+            <img src="${images[count]}" alt="logo habilidad dura">
+        `;
+    }, 3000);
+}
+function main () {
+    handles();
+}
+main();
