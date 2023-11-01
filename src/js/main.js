85e38d6ec9adcbe52a551f7a58563410338ee6e0
Nombre Apellido <email@dominio.com>
2023-11-01 16:09:13 -0500

    add api projects

--- src/js/main.js
+++ src/js/main.js
@@ -25,7 +25,7 @@ function skills () {
         slider.innerHTML = `
             <img src="${images[count]}" alt="logo habilidad dura">
         `;
-    }, 3000);
+    }, 2000);
 }
 function sound () {
     const audio = document.querySelector('.icon__volume');
@@ -79,10 +79,72 @@ function socialMedia () {
         }, 2000);
     });
 }
-function main () {
+async function getApi() {
+    const URL='https://fundametos-api-porfolios-dev-exsn.2.ie-1.fl0.io/api/v1/projects';
+    try {
+        const Data = await fetch(URL);
+        const res = await Data.json();
+        localStorage.setItem('projects', JSON.stringify(res));
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function database () {
+    const db = {
+        projects: JSON.parse(localStorage.getItem('projects')) || await getApi(),
+    }
+    return db;
+}
+function printProjects (projects) {
+    const description = document.querySelectorAll('.splide .splide__slide');
+    const URLS = [
+                'https://yilbernet-poke-api.netlify.app/',
+                'https://yilbernet-rick-and-morty.netlify.app/',
+                'https://yilbernet-weather-app.netlify.app/',
+                ];
+    description.forEach((element, index) => {
+        if (projects[index]) {
+            const { titulo, descripcion, tecnologias, image} = projects[index];
+            let html = `
+                <article class="project">
+                    <h3>${titulo}</h3>
+                    <p><span>Descripción:</span><br>${descripcion}</p>
+                    <p><span>Tecnologías:</span><br>${tecnologias}</p>
+                </article>
+                <a class="project__img" href="${URLS[index]}" target="_blank">
+                    <img src="${image}" alt="img slide">
+                </a>
+            `;
+            element.innerHTML = html;
+        }
+    });
+}
+function splideJs () {
+    let splide = new Splide('.splide', {
+        type: 'loop',
+        // autoplay: true,
+        interval: 3000,
+        perPage: 1,
+        speed: 1000,
+        perMove: 1,
+        gap: 10,
+        breakpoints: {
+            849: {
+                direction: 'ttb',
+                height: '65vh',  
+            },
+        },
+    });
+    splide.mount();
+}
+async function main () {
     skills();
     sound();
     darkMode();
     socialMedia();
+    const db = await database();
+    printProjects(db.projects);
+    splideJs();
 }
 main();
